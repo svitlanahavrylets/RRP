e7e87708132f4096335d3a7e3afae049546c994c
Svitlana Havrylets <svitlana.havrylets@gmail.com>
2025-03-02 18:21:56 +0100

    add vercej.json

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -1,17 +1,21 @@
 import styles from "./HomePage.module.css";
 import Button from "../../components/Button/Button.jsx";
+import heroImage from "../../assets/hero.jpg";
 
 const HomePage = () => {
   return (
     <main>
-      <section className={styles.heroImage}>
+      <section
+        className={styles.heroImage}
+        style={{ backgroundImage: `url(${heroImage})` }}
+      >
         <h1 className={styles.heroTitle}>
           Efektivní řešení pro vaše podnikání
         </h1>
         <Button />
       </section>
-      <section className={styles.heroDescription}>
-        <p>
+      <section className={styles.heroDesContainer}>
+        <p className={styles.heroText}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis,
           minima. Laudantium sint mollitia, rem, qui ea pariatur et placeat
           veniam tempora, officiis beatae atque distinctio quas alias culpa
