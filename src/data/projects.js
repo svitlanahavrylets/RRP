373a0a275eeffb03b61f93bb1e0c18b377ff9377
Svitlana Havrylets <svitlana.havrylets@gmail.com>
2025-03-15 22:49:39 +0100

    projects overlay

---
+++ src/data/projects.js
@@ -0,0 +1,48 @@
+import project1Img from "../assets/proj.jpg";
+import project2Img from "../assets/projekt.jpg";
+
+export const projects = [
+  {
+    title: "Efektivní řízení IT projektů",
+    category: "Projektové řízení v IT",
+    image: project1Img,
+    description:
+      "Specializuji se na řízení IT projektů od plánování až po realizaci. Koordinuji vývojové týmy, optimalizuji procesy a zajišťuji včasné dodání s ohledem na obchodní cíle a technologické trendy.",
+  },
+  {
+    title: "Úspěšné zavádění digitálních řešení",
+    category: "IT management",
+    image: project2Img,
+    description:
+      "Pomáhám firmám implementovat technologická řešení řízením IT projektů ve všech fázích. Definuji požadavky, koordinuji vývoj, testování a nasazení. Zajišťuji hladkou spolupráci týmu a soulad projektu s obchodními cíli.",
+  },
+  {
+    title: "Efektivní řízení IT projektů",
+    category: "Projektové řízení v IT",
+    image: project1Img,
+    description:
+      "Specializuji se na řízení IT projektů od plánování až po realizaci. Koordinuji vývojové týmy, optimalizuji procesy a zajišťuji včasné dodání s ohledem na obchodní cíle a technologické trendy.",
+  },
+  {
+    title: "Efektivní řízení IT projektů",
+    category: "Projektové řízení v IT",
+    image: project1Img,
+    description:
+      "Specializuji se na řízení IT projektů od plánování až po realizaci. Koordinuji vývojové týmy, optimalizuji procesy a zajišťuji včasné dodání s ohledem na obchodní cíle a technologické trendy.",
+  },
+  {
+    title: "Úspěšné zavádění digitálních řešení",
+    category: "IT management",
+    image: project2Img,
+    description:
+      "Pomáhám firmám implementovat technologická řešení řízením IT projektů ve všech fázích. Definuji požadavky, koordinuji vývoj, testování a nasazení. Zajišťuji hladkou spolupráci týmu a soulad projektu s obchodními cíli.",
+  },
+  {
+    title: "Efektivní řízení IT projektů",
+    category: "Projektové řízení v IT",
+    image: project1Img,
+    description:
+      "Specializuji se na řízení IT projektů od plánování až po realizaci. Koordinuji vývojové týmy, optimalizuji procesy a zajišťuji včasné dodání s ohledem na obchodní cíle a technologické trendy.",
+  },
+  // Додати решту проєктів
+];
--- src/page/ProjectsPage/ProjectsPage.jsx
+++ src/page/ProjectsPage/ProjectsPage.jsx
@@ -1,35 +1,19 @@
 import { motion } from "framer-motion";
 import styles from "./ProjectsPage.module.css";
-import project1Img from "../../assets/proj.jpg";
-import project2Img from "../../assets/projekt.jpg";
-
-const projects = [
-  {
-    title: "Efektivní řízení IT projektů",
-    category: "Projektové řízení v IT",
-    image: project1Img,
-    description:
-      "Specializuji se na řízení IT projektů od plánování až po realizaci. Koordinuji vývojové týmy, optimalizuji procesy a zajišťuji včasné dodání s ohledem na obchodní cíle a technologické trendy.",
-  },
-  {
-    title: "Úspěšné zavádění digitálních řešení",
-    category: "IT management",
-    image: project2Img,
-    description:
-      "Pomáhám firmám implementovat technologická řešení řízením IT projektů ve všech fázích. Definuji požadavky, koordinuji vývoj, testování a nasazení. Zajišťuji hladkou spolupráci týmu a soulad projektu s obchodními cíli.",
-  },
-  // Додай решту проєктів
-];
+import { projects } from "../../data/projects.js";
 
 const ProjectsPage = () => {
   return (
-    <section className={styles.portfolioSection} id="our-portfolio">
-      <div className={styles.container}>
+    <section className={styles.portfolioSection}>
+      <div className="container">
         <h2 className={styles.portfolioTitle}>Naše portfolio</h2>
         <ul className={styles.projectCard}>
           {projects.map((project, index) => (
             <li key={index} className={styles.projectCardItem}>
-              <div className={styles.portfolioOverlayImages}>
+              <motion.div
+                className={styles.portfolioOverlayImages}
+                whileHover={{ scale: 1.05 }}
+              >
                 <img
                   src={project.image}
                   alt={project.title}
@@ -37,13 +21,13 @@ const ProjectsPage = () => {
                 />
                 <motion.p
                   className={styles.portfolioOverlayText}
-                  initial={{ opacity: 0 }}
-                  whileHover={{ opacity: 1 }}
+                  initial={{ opacity: 0, y: 100 }}
+                  whileHover={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.3 }}
                 >
                   {project.description}
                 </motion.p>
-              </div>
+              </motion.div>
               <div className={styles.projectCardContainer}>
                 <h3 className={styles.projectCardTitle}>{project.title}</h3>
                 <p className={styles.projectCardText}>{project.category}</p>
