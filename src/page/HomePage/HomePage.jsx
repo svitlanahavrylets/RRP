b9d0760379824fe10bee37ab385dc3578804aab7
Svitlana Havrylets <svitlana.havrylets@gmail.com>
2025-04-07 10:44:54 +0200

    update3 height hero img

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -8,15 +8,26 @@ const HomePage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    const setRealHeight = () => {
+      const height = window.innerHeight;
+      document.documentElement.style.setProperty(
+        "--real-height",
+        `${height}px`
+      );
+      if (window.innerWidth < 768) {
+        const height = document.documentElement.clientHeight;
+        document.documentElement.style.setProperty(
+          "--real-height",
+          `${height}px`
+        );
+      }
     };
 
-    setVh();
-    window.addEventListener("resize", setVh);
+    setRealHeight(); // встановити при першому завантаженні
 
-    return () => window.removeEventListener("resize", setVh);
+    // необов’язково оновлювати при resize, якщо хочеш фіксовану
+    // window.addEventListener("resize", setRealHeight);
+    // return () => window.removeEventListener("resize", setRealHeight);
   }, []);
 
   const handleOpenModal = () => setIsModalOpen(true);
