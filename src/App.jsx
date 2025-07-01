eebf207b727a150d9c65f129a20a018bec3f2ff2
Svitlana Havrylets <svitlana.havrylets@gmail.com>
2025-07-01 13:08:14 +0200

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const HomePage = lazy(() => import("./page/HomePage/HomePage.jsx"));
 const OrderThanksPage = lazy(() =>
   import("./page/OrderThanksPage/OrderThanksPage.jsx")
 );
-// const OurTeamPage = lazy(() => import("./page/OurTeamPage/OurTeamPage.jsx"));
+const OurTeamPage = lazy(() => import("./page/OurTeamPage/OurTeamPage.jsx"));
 const AboutUsPage = lazy(() => import("./page/AboutUsPage/AboutUsPage.jsx"));
 const OurServicesPage = lazy(() =>
   import("./page/OurServicesPage/OurServicesPage.jsx")
@@ -17,7 +17,7 @@ const OurServicesPage = lazy(() =>
 const ServicesItemPage = lazy(() =>
   import("./page/ServicesItemPage/ServicesItemPage.jsx")
 );
-// const ProjectsPage = lazy(() => import("./page/ProjectsPage/ProjectsPage.jsx"));
+const ProjectsPage = lazy(() => import("./page/ProjectsPage/ProjectsPage.jsx"));
 const CareerPage = lazy(() => import("./page/CareerPage/CareerPage.jsx"));
 const CareerPositionPage = lazy(() =>
   import("./page/CareerPositionPage/CareerPositionPage.jsx")
@@ -45,10 +45,10 @@ function App() {
           <Route path="order" element={<HomePage showModal />} />
           <Route path="order-success" element={<OrderThanksPage />} />
           <Route path="about-us" element={<AboutUsPage />} />
-          {/* <Route path="our-team" element={<OurTeamPage />} /> */}
+          <Route path="our-team" element={<OurTeamPage />} />
           <Route path="our-services" element={<OurServicesPage />} />
           <Route path="our-services/:id" element={<ServicesItemPage />} />
-          {/* <Route path="projects" element={<ProjectsPage />} /> */}
+          <Route path="projects" element={<ProjectsPage />} />
           <Route path="careers" element={<CareerPage />} />
           <Route path="careers/:id" element={<CareerPositionPage />} />
           <Route path="blog" element={<BlogPage />} />
