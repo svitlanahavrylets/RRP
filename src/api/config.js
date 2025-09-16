4c6040fae77d002d54ed3c31746fa1c817e79ded
Svitlana Havrylets <svitlana.havrylets@gmail.com>
2025-09-16 08:46:37 +0200

    update config.js, add .env

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,2 @@
-// export const API_URL = "https://rrp-bakend-production.up.railway.app/api";
-
-// export const API_URL = "https://rrp-bakend.onrender.com/api";
-
-export const API_URL = "http://localhost:5000/api";
+export const API_URL =
+  import.meta.env.VITE_API_URL ?? "http://localhost:5000/api";
